b31be202c3b1e42e76214f7a65846db8adcda0cf
John Pettit <john.pettit@tridonic.com>
2017-06-14 16:08:02 -0700

    first commit

---
+++ src/main/java/boottest/HelloController.java
@@ -0,0 +1,19 @@
+package boottest;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping("/pulse")
+    public String pulse() {
+        return "Greetings from Spring Pulse!";
+    }
+
+}
